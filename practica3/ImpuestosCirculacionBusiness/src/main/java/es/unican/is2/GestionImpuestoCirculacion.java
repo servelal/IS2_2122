55ee8cb6cea5a63f520dea943e108f8762c04d39
Velar <60737813+servelal@users.noreply.github.com>
2022-03-10 13:03:54 +0100

    Cambio

---
+++ practica3/ImpuestosCirculacionBusiness/src/main/java/es/unican/is2/GestionImpuestoCirculacion.java
@@ -0,0 +1,48 @@
+package es.unican.is2;
+/**
+ * Clase que implementa la capa de negocio de la aplicacion
+ */
+public class GestionImpuestoCirculacion implements IGestionContribuyentes, IGestionVehiculos, IInfoImpuestoCirculacion {
+	
+	private IContribuyentesDAO contribuyentes;
+	private IVehiculosDAO vehiculos;
+	
+	public GestionImpuestoCirculacion(IContribuyentesDAO contribuyentes, IVehiculosDAO vehiculos) {
+		this.contribuyentes = contribuyentes;
+		this.vehiculos = vehiculos;
+	}
+	
+	public Contribuyente altaContribuyente(Contribuyente c) {
+		// TODO
+		return null;
+	}
+
+	
+	public Contribuyente bajaContribuyente(String dni) throws OperacionNoValida {
+		// TODO
+		return null;		
+	 }
+	
+	public Contribuyente contribuyente(String dni) {
+		// TODO
+		return null;
+	}
+
+	public Vehiculo altaVehiculo(Vehiculo v, String dni) throws OperacionNoValida {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Vehiculo bajaVehiculo(String matricula, String dni) throws OperacionNoValida {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Vehiculo vehiculo(String matricula) {
+		// TODO
+		return null;
+	}	
+}
+
---
+++ practica3/ImpuestosCirculacionCommon/src/main/java/es/unican/is2/Motocicleta.java
@@ -0,0 +1,21 @@
+package es.unican.is2;
+@SuppressWarnings("serial")
+public class Motocicleta extends Vehiculo
+{
+	private int cilindrada;
+
+    /**
+     * Retorna la cilindrada de la motocicleta
+     * @return cilindrada
+     */
+    public int getCilindrada() {
+        return cilindrada;
+    }
+    
+  
+	@Override
+    public double precioImpuesto() {
+		//TODO
+		return 0;
+    }
+}
