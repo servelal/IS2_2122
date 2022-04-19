946b5f1c8eb6892f330f878cf69fc6595c252fea
iglesiash <hectorglezigl@gmail.com>
2022-04-19 19:59:02 +0200

    package de cada artefacto

--- practica4/compile.sh
+++ practica4/compile.sh
@@ -2,10 +2,12 @@
 # @author Hector Gonzalez Iglesias
 # Script (para linux) que automaticamente compila e instala los artefactos necesitados en orden
 
-for i in ImpuestosCirculacionCommon ImpuestosCirculacionDAO ImpuestosCirculacionBusiness ImpuestosCirculacionGUI ImpuestosCirculacionMain ImpuestosCirculacionRellena
+for i in ImpuestosCirculacionCommon ImpuestosCirculacionDAO ImpuestosCirculacionBusiness ImpuestosCirculacionGUI ImpuestosCirculacionMain ImpuestosCirculacionRellena ListaAcotada
 do
 	cd $i/
-	mvn compile
-	mvn install
+	mvn clean validate
+	mvn clean compile
+	mvn clean install
+	mvn clean package
 	cd ..
 done
