cb01c76a660d469125f674105eada72a1a56f3ce
iglesiash <hgi834@alumnos.unican.es>
2022-04-28 13:25:07 +0200

    NOC y CCog Tienda

--- practica5/src/main/java/es/unican/is2/gestionTienda/Tienda.java
+++ practica5/src/main/java/es/unican/is2/gestionTienda/Tienda.java
@@ -14,9 +14,11 @@ import java.util.Scanner;
  * Clase que representa una tienda con un conjunto de vendedores.
  * Gestiona las ventas realizadas y las comisiones asignadas a cada
  * vendedor. Los datos de la tienda se almacenan en un fichero de texto
- * que se pasa como parámetro al crear la tienda
+ * que se pasa como parï¿½metro al crear la tienda
  */
 public class Tienda {
+	// NOC = 0
+	// CCog = 0 + 0 + 0 + 1 + 1 + 4 + 8 + 5 + 11 = 30
 
 	private LinkedList<Vendedor> lista = new LinkedList<Vendedor>();
 	private String direccion;
@@ -29,15 +31,15 @@ public class Tienda {
 	 * 
 	 * @param datos Path absoluto del fichero de datos
 	 */
-	public Tienda(String datos) {
+	public Tienda(String datos) { //CCog 0
 		this.datos = datos;
 	}
 
 	/**
-	 * Retorna la dirección de la tienda
-	 * @return Dirección de la tienda
+	 * Retorna la direcciï¿½n de la tienda
+	 * @return Direcciï¿½n de la tienda
 	 */
-	public String direccion() {
+	public String direccion() {//CCog 0
 		return direccion;
 	}
 
@@ -45,20 +47,20 @@ public class Tienda {
 	 * Retorna el nombre de la tienda
 	 * @return Nombre de la tienda
 	 */
-	public String nombre() {
+	public String nombre() { //CCog 0
 		return nombre;
 	}
 
 	/**
-	 * Añade un nuevo vendedor a la tienda
+	 * Aï¿½ade un nuevo vendedor a la tienda
 	 * 
 	 * @param nuevoVendedor
 	 * @return true si el vendedor se ha anhadido 
-	 *         false si ya había un vendedor con el mismo id
+	 *         false si ya habï¿½a un vendedor con el mismo id
 	 */
-	public boolean anhade(Vendedor nuevoVendedor) throws IOException {
+	public boolean anhade(Vendedor nuevoVendedor) throws IOException { //Ccog 1
 		Vendedor v = buscaVendedor(nuevoVendedor.getId());
-		if (v != null) {
+		if (v != null) { //Ccog + 1
 			return false;
 		}
 		lista.add(nuevoVendedor);
@@ -67,15 +69,15 @@ public class Tienda {
 	}
 
 	/**
-	 * Elimina el vendedor cuyo dni se pasa como parámetro
+	 * Elimina el vendedor cuyo dni se pasa como parï¿½metro
 	 * 
 	 * @param id
 	 * @return true si se elimina el vendedor 
-	 *         false si no existe ningún vendedor con el id indicado
+	 *         false si no existe ningï¿½n vendedor con el id indicado
 	 */
-	public boolean eliminaVendedor(String id) throws IOException {
+	public boolean eliminaVendedor(String id) throws IOException { //Ccog 1
 		Vendedor v = buscaVendedor(id);
-		if (v == null) {
+		if (v == null) { //CCog + 1
 			return false;
 		}
 		lista.remove(v);
@@ -84,20 +86,20 @@ public class Tienda {
 	}
 
 	/**
-	 * Añade una venta a un vendedor
+	 * Aï¿½ade una venta a un vendedor
 	 * @param id Id del vendedor
 	 * @param importe Importe de la venta
-	 * @return true si se añade la venta 
+	 * @return true si se aï¿½ade la venta 
 	 *         false si no se encuentra el vendedor
 	 */
-	public boolean anhadeVenta(String id, double importe) throws IOException {
+	public boolean anhadeVenta(String id, double importe) throws IOException { //Ccog 4
 		Vendedor v = buscaVendedor(id);
-		if (v == null) {
+		if (v == null) { // Ccog + 1
 			return false;
 		}
-		double importeFinal = importe;
-		if (v instanceof VendedorEnPlantilla) {
-			switch (((VendedorEnPlantilla) v).tipo()) {
+		double importeFinal = importe; 
+		if (v instanceof VendedorEnPlantilla) { //CCog +1
+			switch (((VendedorEnPlantilla) v).tipo()) { //CCog +2
 			case JUNIOR:
 				importeFinal += importeFinal * 0.005;
 				break;
@@ -117,21 +119,21 @@ public class Tienda {
 	 * @param id Id del vendedor
 	 * @return vendedor con ese id o null si no existe ninguno
 	 */
-	public Vendedor buscaVendedor(String id) {
+	public Vendedor buscaVendedor(String id) { //Ccog 8
 
 		lista = new LinkedList<Vendedor>();
 		Scanner in = null;
 		try {
 			// abre el fichero
 			in = new Scanner(new FileReader(datos));
-			// configura el formato de números
+			// configura el formato de nï¿½meros
 			in.useLocale(Locale.ENGLISH);
 			nombre = in.nextLine();
 			direccion = in.nextLine();
 			in.next();
 			Vendedor ven = null;
 			// lee los vendedores senior
-			while (in.hasNext() && !in.next().equals("Junior")) {
+			while (in.hasNext() && !in.next().equals("Junior")) { // Ccog + 2
 
 				String nombre = in.next();
 				in.next();
@@ -145,7 +147,7 @@ public class Tienda {
 				lista.add(ven);
 			}
 			// lee los vendedores junior
-			while (in.hasNext() && !in.next().equals("Prácticas")) {
+			while (in.hasNext() && !in.next().equals("Prï¿½cticas")) { // Ccog + 2
 				String nombre = in.next();
 				in.next();
 				String idIn = in.next();
@@ -157,7 +159,7 @@ public class Tienda {
 				ven.setT(totalVentas);
 				lista.add(ven);
 			}
-			while (in.hasNext()) {
+			while (in.hasNext()) { // Ccog + 1
 				in.next();
 				String nombre = in.next();
 				in.next();
@@ -177,8 +179,8 @@ public class Tienda {
 			}
 		} // try
 
-		for (Vendedor v : lista) {
-			if (v.getId().equals(id)) {
+		for (Vendedor v : lista) { // Ccog + 1
+			if (v.getId().equals(id)) { // Ccog + 2
 				return v;
 			}
 		}
@@ -189,21 +191,21 @@ public class Tienda {
 	 * Retorna la lista de vendedores actuales de la tienda 
 	 * @return La lista de vendedores
 	 */
-	public List<Vendedor> vendedores() {
+	public List<Vendedor> vendedores() { //Ccog 5
 		lista = new LinkedList<Vendedor>();
 
 		Scanner in = null;
 		try {
 			// abre el fichero
 			in = new Scanner(new FileReader(datos));
-			// configura el formato de números
+			// configura el formato de nï¿½meros
 			in.useLocale(Locale.ENGLISH);
 			nombre = in.nextLine();
 			direccion = in.nextLine();
 			in.next();
 			Vendedor ven = null;
 			// lee los vendedores senior
-			while (in.hasNext() && !in.next().equals("Junior")) {
+			while (in.hasNext() && !in.next().equals("Junior")) { // Ccog + 2
 
 				String nombre = in.next();
 				in.next();
@@ -217,7 +219,7 @@ public class Tienda {
 				lista.add(ven);
 			}
 			// lee los vendedores junior
-			while (in.hasNext() && !in.next().equals("Prácticas")) {
+			while (in.hasNext() && !in.next().equals("Prï¿½cticas")) { // Ccog + 2
 				String nombre = in.next();
 				in.next();
 				String idIn = in.next();
@@ -229,7 +231,7 @@ public class Tienda {
 				ven.setT(totalVentas);
 				lista.add(ven);
 			}
-			while (in.hasNext()) {
+			while (in.hasNext()) { // Ccog + 1
 				in.next();
 				String nombre = in.next();
 				in.next();
@@ -255,21 +257,21 @@ public class Tienda {
 	}
 
 	/**
-	 * Método que actualiza el fichero datosTienda.txt 
+	 * Mï¿½todo que actualiza el fichero datosTienda.txt 
 	 * con los datos actualizados de los vendedores
 	 */
-	private void vuelcaDatos() throws IOException {
+	private void vuelcaDatos() throws IOException { //Ccog 11
 		PrintWriter out = null;
 		List<Vendedor> senior = new LinkedList<Vendedor>();
 		List<Vendedor> junior = new LinkedList<Vendedor>();
 		List<Vendedor> practicas = new LinkedList<Vendedor>();
 
-		for (Vendedor v : lista) {
-			if (v instanceof vendedorEnPracticas) {
+		for (Vendedor v : lista) { // Ccog + 1
+			if (v instanceof vendedorEnPracticas) { // Ccog + 2
 				practicas.add(v);
-			} else if (v instanceof VendedorEnPlantilla) {
+			} else if (v instanceof VendedorEnPlantilla) { // Ccog + 2
 				VendedorEnPlantilla vp = (VendedorEnPlantilla) v;
-				if (vp.tipo().equals(TipoVendedor.JUNIOR))
+				if (vp.tipo().equals(TipoVendedor.JUNIOR)) // Ccog + 3
 					junior.add(vp);
 				else
 					senior.add(vp);
@@ -284,21 +286,21 @@ public class Tienda {
 			out.println(direccion);
 			out.println();
 			out.println("Senior");
-			for (Vendedor v : senior) {
+			for (Vendedor v : senior) { // Ccog + 1
 				VendedorEnPlantilla v1 = (VendedorEnPlantilla) v;
 				out.println("  Nombre: " + v1.getNombre() + " Id: " + v1.getId() + " DNI: "+ v1.getDni()+" TotalVentasMes: "
 						+ v1.getTotalVentas());
 			}
 			out.println();
 			out.println("Junior");
-			for (Vendedor v : junior) {
+			for (Vendedor v : junior) { // Ccog + 1
 				VendedorEnPlantilla v2 = (VendedorEnPlantilla) v;
 				out.println("  Nombre: " + v2.getNombre() + " Id: " + v2.getId() + " DNI: "+ v2.getDni()+" TotalVentasMes: "
 						+ v2.getTotalVentas());
 			}
 			out.println();
-			out.println("Prácticas");
-			for (Vendedor v : practicas) {
+			out.println("Prï¿½cticas");
+			for (Vendedor v : practicas) { // Ccog + 1
 				vendedorEnPracticas v3 = (vendedorEnPracticas) v;
 				out.println("  Nombre: " + v3.getNombre() + " Id: " + v3.getId() + " DNI: "+ v3.getDni()+" TotalVentasMes: "
 						+ v3.getTotalVentas());
